e8e754e9cf034ca8aa8eb2e0e13be97e7869c799
pedro <parmeggianipedro@gmail.com>
2024-09-13 21:25:54 -0300

    removed namespace

--- shared/archivos.h
+++ shared/archivos.h
@@ -1,43 +1,39 @@
 #include "iarchivos.h"
-#include <iostream>
 
-namespace archivos
+FILE *abrir_archivo_lectura(char *path)
 {
-    FILE *abrir_archivo_lectura(char *path)
-    {
-        return abrir_archivo(path, "r");
-    }
-
-    FILE *abrir_archivo_escritura(char *path)
-    {
-        return abrir_archivo(path, "w");
-    }
-
-    FILE *append_archivo(char *path)
-    {
-        return abrir_archivo(path, "a");
-    }
-
-    FILE *abrir_archivo(char *path, char *modo)
-    {
-        logInfo("Abriendo archivo.");
-        FILE *fp;
-        fp = fopen(path, modo);
-
-        if (fp == NULL)
-        {
-            logErr("No se encontró puntero para archivo.");
-            exit(0);
-        }
-        return fp;
-    }
-
-    void cerrar_archivo(FILE *fp)
-    {
-        logInfo("Cerrando archivo.");
-        fclose(fp);
-    }
-
-    // Leer archivo completo
-    // append al archivo
+    return abrir_archivo(path, "r");
 }
+
+FILE *abrir_archivo_escritura(char *path)
+{
+    return abrir_archivo(path, "w");
+}
+
+FILE *append_archivo(char *path)
+{
+    return abrir_archivo(path, "a");
+}
+
+FILE *abrir_archivo(char *path, char *modo)
+{
+    logInfo("Abriendo archivo.");
+    FILE *fp;
+    fp = fopen(path, modo);
+
+    if (fp == NULL)
+    {
+        logErr("No se encontró puntero para archivo.");
+        exit(0);
+    }
+    return fp;
+}
+
+void cerrar_archivo(FILE *fp)
+{
+    logInfo("Cerrando archivo.");
+    fclose(fp);
+}
+
+// Leer archivo completo
+// append al archivo
--- shared/funciones.h
+++ shared/funciones.h
@@ -1,154 +1,150 @@
 #include "ifunciones.h"
-#include <iostream>
 
-namespace funciones
+key_t creo_clave(int r_clave)
 {
-    key_t creo_clave(int r_clave)
-    {
-        key_t clave;
-        clave = ftok("/bin/ls", r_clave);
+    key_t clave;
+    clave = ftok("/bin/ls", r_clave);
 
-        if (clave == (key_t)-1)
-        {
-            printf("Error: no se puede conseguir clave para memoria compartida\n");
-            exit(0);
-        }
-        return clave;
+    if (clave == (key_t)-1)
+    {
+        printf("Error: no se puede conseguir clave para memoria compartida\n");
+        exit(0);
+    }
+    return clave;
+}
+
+void *creo_memoria(int size, int *r_id_memoria, int clave_base)
+{
+    void *ptr_memoria;
+    int id_memoria;
+    id_memoria = shmget(creo_clave(clave_base), size, 0777 | IPC_CREAT);
+
+    if (id_memoria == -1)
+    {
+        printf("Error: no se puede conseguir memoria compartida\n");
+        exit(0);
     }
 
-    void *creo_memoria(int size, int *r_id_memoria, int clave_base)
+    ptr_memoria = (void *)shmat(id_memoria, (char *)0, 0);
+    if (ptr_memoria == NULL)
     {
-        void *ptr_memoria;
-        int id_memoria;
-        id_memoria = shmget(creo_clave(clave_base), size, 0777 | IPC_CREAT);
-
-        if (id_memoria == -1)
-        {
-            printf("Error: no se puede conseguir memoria compartida\n");
-            exit(0);
-        }
-
-        ptr_memoria = (void *)shmat(id_memoria, (char *)0, 0);
-        if (ptr_memoria == NULL)
-        {
-            printf("Error: no se puede conseguir memoria compartida\n");
-            exit(0);
-        }
-        *r_id_memoria = id_memoria;
-        return ptr_memoria;
+        printf("Error: no se puede conseguir memoria compartida\n");
+        exit(0);
     }
+    *r_id_memoria = id_memoria;
+    return ptr_memoria;
+}
 
-    int creo_semaforo(int cantidad)
+int creo_semaforo(int cantidad)
+{
+    key_t clave = creo_clave(CLAVE_BASE);
+    int id_semaforo = semget(clave, cantidad, 0600 | IPC_CREAT);
+    if (id_semaforo == -1)
     {
-        key_t clave = creo_clave(CLAVE_BASE);
-        int id_semaforo = semget(clave, cantidad, 0600 | IPC_CREAT);
-        if (id_semaforo == -1)
-        {
-            printf("Error: no se puede crear semaforo \n");
-            exit(0);
-        }
-        return id_semaforo;
+        printf("Error: no se puede crear semaforo \n");
+        exit(0);
     }
+    return id_semaforo;
+}
 
-    void inicio_semaforo(int id, int cual, int valor)
+void inicio_semaforo(int id, int cual, int valor)
+{
+    semctl(id, cual, SETVAL, valor);
+}
+
+void sem_oper(int id, int cual, int op)
+{
+    struct sembuf operacion;
+    operacion.sem_num = cual;
+    operacion.sem_op = op;
+    operacion.sem_flg = 0;
+    semop(id, &operacion, 1);
+}
+
+void levanta_semaforo(int id, int cual)
+{
+    sem_oper(id, cual, 1);
+}
+
+void espera_semaforo(int id, int cual)
+{
+    sem_oper(id, cual, -1);
+}
+
+int creo_id_cola_mensajes(int clave)
+{
+    int id_cola = msgget(creo_clave(clave), 0600 | IPC_CREAT);
+    if (id_cola == -1)
     {
-        semctl(id, cual, SETVAL, valor);
+        printf("Error: no se pudo crear identificador para cola mensajes\n");
+        exit(0);
     }
+    return id_cola;
+}
 
-    void sem_oper(int id, int cual, int op)
+int enviar_mensaje(int id_cola, long dest, int rte, int evento, char *message)
+{
+    mensaje msg;
+    msg.long_dest = dest;
+    msg.int_rte = rte;
+    msg.int_evento = evento;
+    strcpy(msg.char_mensaje, message);
+    return msgsnd(id_cola, (struct msgbuf *)&msg, sizeof(msg.int_rte) + sizeof(msg.int_evento) + sizeof(msg.char_mensaje), IPC_NOWAIT);
+}
+
+int recibir_mensajes(int id_cola, long dest, mensaje *message)
+{
+    mensaje msg;
+    int res;
+    res = msgrcv(id_cola, (struct msgbuf *)&msg, sizeof(msg.int_rte) + sizeof(msg.int_evento) + sizeof(msg.char_mensaje), dest, 0);
+
+    message->long_dest = msg.long_dest;
+    message->int_rte = msg.int_rte;
+    message->int_evento = msg.int_evento;
+    strcpy(message->char_mensaje, msg.char_mensaje);
+
+    return res;
+}
+
+int borrar_mensajes(int id_cola)
+{
+    mensaje msg;
+    int res;
+    do
     {
-        struct sembuf operacion;
-        operacion.sem_num = cual;
-        operacion.sem_op = op;
-        operacion.sem_flg = 0;
-        semop(id, &operacion, 1);
-    }
+        res = msgrcv(id_cola, (struct msgbuf *)&msg, sizeof(msg.int_rte) + sizeof(msg.int_evento) + sizeof(msg.char_mensaje), 0, IPC_NOWAIT);
+    } while (res > 0);
+    return res;
+}
 
-    void levanta_semaforo(int id, int cual)
+void printLn()
+{
+    printf("---------------------------------\n");
+}
+
+void logErr(char *message)
+{
+    if (LOG_LEVEL == NONE)
     {
-        sem_oper(id, cual, 1);
+        return;
     }
+    printf("\033[31mError\033[0m: %s\n", message);
+}
 
-    void espera_semaforo(int id, int cual)
+void logInfo(char *message)
+{
+    if (LOG_LEVEL == NONE || LOG_LEVEL == WARN || LOG_LEVEL == ERR)
     {
-        sem_oper(id, cual, -1);
+        return;
     }
+    printf("\033[32mInfo\033[0m: %s\n", message);
+}
 
-    int creo_id_cola_mensajes(int clave)
+void logWarn(char *message)
+{
+    if (LOG_LEVEL == NONE || LOG_LEVEL == ERR)
     {
-        int id_cola = msgget(creo_clave(clave), 0600 | IPC_CREAT);
-        if (id_cola == -1)
-        {
-            printf("Error: no se pudo crear identificador para cola mensajes\n");
-            exit(0);
-        }
-        return id_cola;
+        return;
     }
-
-    int enviar_mensaje(int id_cola, long dest, int rte, int evento, char *message)
-    {
-        mensaje msg;
-        msg.long_dest = dest;
-        msg.int_rte = rte;
-        msg.int_evento = evento;
-        strcpy(msg.char_mensaje, message);
-        return msgsnd(id_cola, (struct msgbuf *)&msg, sizeof(msg.int_rte) + sizeof(msg.int_evento) + sizeof(msg.char_mensaje), IPC_NOWAIT);
-    }
-
-    int recibir_mensajes(int id_cola, long dest, mensaje *message)
-    {
-        mensaje msg;
-        int res;
-        res = msgrcv(id_cola, (struct msgbuf *)&msg, sizeof(msg.int_rte) + sizeof(msg.int_evento) + sizeof(msg.char_mensaje), dest, 0);
-
-        message->long_dest = msg.long_dest;
-        message->int_rte = msg.int_rte;
-        message->int_evento = msg.int_evento;
-        strcpy(message->char_mensaje, msg.char_mensaje);
-
-        return res;
-    }
-
-    int borrar_mensajes(int id_cola)
-    {
-        mensaje msg;
-        int res;
-        do
-        {
-            res = msgrcv(id_cola, (struct msgbuf *)&msg, sizeof(msg.int_rte) + sizeof(msg.int_evento) + sizeof(msg.char_mensaje), 0, IPC_NOWAIT);
-        } while (res > 0);
-        return res;
-    }
-
-    void printLn()
-    {
-        printf("---------------------------------\n");
-    }
-
-    void logErr(char *message)
-    {
-        if (LOG_LEVEL == NONE)
-        {
-            return;
-        }
-        printf("\033[31mError\033[0m: %s\n", message);
-    }
-
-    void logInfo(char *message)
-    {
-        if (LOG_LEVEL == NONE || LOG_LEVEL == WARN || LOG_LEVEL == ERR)
-        {
-            return;
-        }
-        printf("\033[32mInfo\033[0m: %s\n", message);
-    }
-
-    void logWarn(char *message)
-    {
-        if (LOG_LEVEL == NONE || LOG_LEVEL == ERR)
-        {
-            return;
-        }
-        printf("\033[33mWarn\033[0m: %s\n", message);
-    }
+    printf("\033[33mWarn\033[0m: %s\n", message);
 }
