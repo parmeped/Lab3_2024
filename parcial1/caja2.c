c10dcb128c8bd6ab7524bb956ba73b9cf212fd0a
pedro <parmeggianipedro@gmail.com>
2024-09-19 21:09:09 -0300

    pushing changes

---
+++ parcial1/caja2.c
@@ -0,0 +1,49 @@
+#include "../shared/framework.h"
+#include "def.h"
+
+int main(int argc, char *argv[])
+{
+    char print_message[LARGO_MENSAJE];
+    
+    int caj_id = atoi(argv[1]);
+    
+    logInfo("No proceso compras mayores a 20000");
+
+    FILE *fp;
+
+    int id_semaforo = creo_semaforo(SEM_AMOUNT);
+    char nombre_archivo[100];
+    int total = 0;
+
+    while(1) 
+    {
+        espera_semaforo(id_semaforo, SEM_NUMBER);
+        
+        sprintf(print_message, "Procesando compra en caja %d", caj_id);
+        logInfo(print_message);
+
+        fp = abrir_archivo_lectura(NOMBRE_ARCHIVO);
+        if (fp == NULL) 
+        {
+            logErr("No se pudo abrir archivo para lectura");
+            libero_semaforo_mspinner(id_semaforo, SEM_NUMBER, SLEEP_CAJA_MS);
+            continue;
+        }
+
+        fscanf(fp, ARCHIVO_LINE, &total);
+        
+        if (total <= AMOUNT_PROCESS) 
+        {
+            libero_semaforo_mspinner(id_semaforo, SEM_NUMBER, SLEEP_CAJA_MS);
+        }
+        else
+        {
+            logErr("Procesando compra...");
+            sprintf(print_message, "Leido total %d por %d", total, caj_id);
+            logInfo(print_message);
+            borrar_archivo(NOMBRE_ARCHIVO);
+            libero_semaforo_mspinner(id_semaforo, SEM_NUMBER, SLEEP_CAJA_MS);
+        }       
+    }
+    return 0;
+}
