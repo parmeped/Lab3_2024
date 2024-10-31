65a33a32432ee1be4c46522311abff81cfd90634
pedro <parmeggianipedro@gmail.com>
2024-10-31 20:56:25 -0300

    asd

--- parcial2/tableroE.c
+++ parcial2/tableroE.c
@@ -59,9 +59,18 @@ int main()
 		spinner(1);
 	}
 
+	printf("mosquitos: %d\n", mosquitos);
+
 	logInfo("Iniciando Carrera");
 	while(memoriaStatus->run == 1)
 	{
+		if (mosquitos <= 0) 
+		{
+			logWarn("Gano el humo!");
+			memoriaStatus->run = 0;
+			break;
+		}
+
 		logInfof("Iniciando turno %d", turno++);
 		for (i = 0; i < runners_amount; i++)
 		{
@@ -70,13 +79,6 @@ int main()
 				printf("enviando a %d", memoria[i].runner);
 				enviar_mensaje(id_cola_mensajes, memoria[i].runner, MSG_POINTER, EVT_CORRO, "");
 			}
-		}
-
-		if (mosquitos <= 0) 
-		{
-			logWarn("Gano el humo!");
-			memoriaStatus->run = 0;
-			break;
 		}
 
 		if (memoria[MSG_SMOKE].totalSteps >= memoria[MSG_MOSQUITO1].totalSteps)
