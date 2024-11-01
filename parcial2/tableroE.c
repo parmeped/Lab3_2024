3c483b7ff8e2b8aa6e6b775ae6682ee40156a3f8
pedro <parmeggianipedro@gmail.com>
2024-10-31 21:20:27 -0300

    asd

--- parcial2/tableroE.c
+++ parcial2/tableroE.c
@@ -75,6 +75,8 @@ int main()
 		logInfof("Iniciando turno %d", turno++);
 		for (i = 0; i < runners_amount; i++)
 		{
+			printf("alive: %d\n", i, memoria[i].alive);
+			printf("runner: %d\n", i, memoria[i].runner);
 			if (memoria[i].alive != 0)
 			{
 				printf("enviando a %d", memoria[i].runner);
