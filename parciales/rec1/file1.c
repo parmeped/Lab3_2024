87b7db62dcb1a078c93564fd2ed0a7edd0b6b7af
pedro <parmeggianipedro@gmail.com>
2024-11-14 18:00:22 -0300

    pushingl atest changes

---
+++ parciales/rec1/file1.c
@@ -0,0 +1,130 @@
+
+#include "def.h"
+#include "../shared/framework.h"
+
+#define LEO 1
+#define ESCRIBO 2
+
+FILE *panelFile;
+
+panel paneles[MAX_PANNELS];
+
+void fileSetup()
+{
+    panelFile = abrir_archivo_escritura(FILE_PATH);
+    for (int i = 0; i < MAX_PANNELS; i++)
+    {
+        paneles[i].nro_panel = i;
+        strcpy(paneles[i].mensaje, DEFAULT_MESSAGE);
+        fwrite(&paneles[i], sizeof(panel), 1, panelFile);
+    }
+    cerrar_archivo(panelFile);
+}
+
+int panelNumberMenu() 
+{
+    // ask for panel number, check if input is number, else rerun input request
+    int panelNumber;
+    printf("Ingrese el numero de panel: ");
+    while (scanf("%d", &panelNumber) != 1)
+    {
+        printf("Numero de panel invalido, intente de nuevo: ");
+        fflush(stdin);
+    }
+    // check if panel number is valid
+    if (panelNumber < 1 || panelNumber > MAX_PANNELS)
+    {
+        printf("Numero de panel invalido, intente de nuevo: ");
+        fflush(stdin);        
+    }
+    return panelNumber--;
+}
+
+void leo()
+{
+    int panelNumber = panelNumberMenu();
+    
+    panelFile = abrir_archivo_lectura(FILE_PATH);
+
+    fseek(panelFile, panelNumber * sizeof(panel), SEEK_SET);
+    
+    fread(&paneles[panelNumber], sizeof(panel), 1, panelFile);
+    printf("Panel %d: %s\n", paneles[panelNumber].nro_panel, paneles[panelNumber].mensaje);
+    cerrar_archivo(panelFile);
+}
+
+void escribo()
+{
+    int panelNumber = panelNumberMenu();
+
+    panelFile = abrir_archivo_escritura(FILE_PATH);    
+
+    fseek(panelFile, panelNumber * sizeof(panel), SEEK_SET);
+
+    paneles[panelNumber].nro_panel = panelNumber;
+    printf("Ingrese el mensaje: ");
+    scanf("%s", paneles[panelNumber].mensaje);
+    fwrite(&paneles[panelNumber], sizeof(panel), 1, panelFile);
+    cerrar_archivo(panelFile);
+}
+
+int menu(void)
+{
+    int opcion;
+
+    printf("Menu\n\n");
+    printf("1.\tLeer Panel\n");
+    printf("2.\tEscribo mensaje\n");
+    printf("0.\tSalir\n\n");
+    printf("Elija opcion: ");
+    fflush(stdin);  
+    while( (scanf(" %d", &opcion) != 1)
+          || (opcion < 0)            
+          || (opcion > MAX_MENU))    
+    {
+      fflush(stdin);                 
+      printf("No valido\n\n");
+      printf("Elija opcion: ");
+    }
+    return opcion;
+}
+
+int main()
+{
+	char cadena[LARGO_MENSAJE];
+
+	int id_semaforo, opcion;
+	
+	id_semaforo = creo_semaforo(CANT_SEMAFORO);
+	inicio_semaforo(id_semaforo, CUAL_SEMAFORO, VERDE);
+	
+    fileSetup();
+
+	opcion = menu();
+
+	while(opcion != SALIR)
+	{
+		switch(opcion)
+		{
+			case LEO: 
+                espera_semaforo(id_semaforo, CUAL_SEMAFORO);
+                    leo();
+                libero_semaforo(id_semaforo, CUAL_SEMAFORO);
+                break;
+			case ESCRIBO: 
+                espera_semaforo(id_semaforo, CUAL_SEMAFORO);
+                    escribo();
+                libero_semaforo(id_semaforo, CUAL_SEMAFORO);
+			    break;
+			case SALIR: 
+			    printf("Salir:\n");
+			    break;
+			default:    
+			    printf("Opcion no valida\n");
+		}
+
+        usleepMs(TIEMPO_SLEEP);
+		opcion = menu(); 
+	}
+	return 0;
+}
---
+++ parciales/rec1/file2.c
@@ -0,0 +1,51 @@
+#include "def.h"
+#include "../shared/framework.h"
+
+#define TIEMPO_SLEEP_PANEL 100
+
+// store current message
+panel pannel;
+
+void leo(int panelId)
+{
+    FILE* panelFile;
+    panelFile = abrir_archivo_lectura(FILE_PATH);
+    panel tempPanel;
+    fseek(panelFile, panelId * sizeof(panel), SEEK_SET);
+    fread(&tempPanel, sizeof(panel), 1, panelFile);
+    
+    // only print if panel message is different than stored one
+    if (strcmp(tempPanel.mensaje, pannel.mensaje) != 0)
+    {
+        strcpy(pannel.mensaje, tempPanel.mensaje);
+        printf("Panel %d: %s\n", tempPanel.nro_panel, tempPanel.mensaje);
+    }
+    cerrar_archivo(panelFile);
+}
+
+// receive panel id through args
+int main(int argc, char *argv[])
+{
+    
+    // check for panelId argument, try to parse to number
+    int panelId = atoi(argv[1]);
+    if (panelId < 1) 
+    {
+        logInfo("Valor invalido de id para panel");
+        return 1;
+    }
+    int id_semaforo = creo_semaforo(CANT_SEMAFORO);
+
+    logInfof("Iniciando panel: %d", panelId);
+    panelId--;
+
+    while(1) 
+    {
+        espera_semaforo(id_semaforo, CUAL_SEMAFORO);
+            leo(panelId);
+        libero_semaforo(id_semaforo, CUAL_SEMAFORO);
+        usleepMs(TIEMPO_SLEEP_PANEL);
+    }
+    
+    return 0;
+}
