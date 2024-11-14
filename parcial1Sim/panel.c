0b8029995340fef6597d35f56661e1e87cfb881f
pedro <parmeggianipedro@gmail.com>
2024-11-13 21:10:40 -0300

    tes

--- parcial1Sim/panel.c
+++ parcial1Sim/panel.c
@@ -10,13 +10,14 @@ panel pannel;
 void leo(int *panelId)
 {
     panelFile = abrir_archivo_lectura(FILE_PATH);
+    panel tempPanel;
     fseek(panelFile, *panelId * sizeof(panel), SEEK_SET);
-    fread(&paneles[*panelId], sizeof(panel), 1, panelFile);
+    fread(&tempPanel, sizeof(panel), 1, panelFile);
     // only print if panel message is different than stored one
-    if (strcmp(paneles[*panelId].mensaje, pannel.mensaje) != 0)
+    if (strcmp(tempPanel.mensaje, pannel.mensaje) != 0)
     {
-        strcpy(pannel.mensaje, paneles[*panelId].mensaje);
-        printf("Panel %d: %s\n", paneles[*panelId].nro_panel, paneles[*panelId].mensaje);
+        strcpy(pannel.mensaje, tempPanel.mensaje);
+        printf("Panel %d: %s\n", tempPanel.nro_panel, tempPanel.mensaje);
     }
     cerrar_archivo(panelFile);
 }
